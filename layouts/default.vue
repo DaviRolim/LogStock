0736e3cbec05d79e5369914c2ad0ab15d217e260
MVREC\davi.holanda <davi.holanda>
2018-04-16 13:57:34 -0300

    initial Commit

---
+++ layouts/default.vue
@@ -0,0 +1,79 @@
+<template>
+  <v-app dark>
+    <v-navigation-drawer fixed temporary app v-model="sideNav" >
+      <v-list>
+        <v-list-tile :to="item.to" v-for="(item,index) in menuItems" :key="index">
+          <v-list-tile-action>
+            <v-icon>{{item.icon}}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>{{item.title}}</v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile v-if="userIsAuthenticated" @click="onLogout">
+          <v-list-tile-action>
+            <v-icon>exit_to_app</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>Logout</v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar>
+      <v-toolbar-side-icon @click.stop="sideNav = !sideNav" class="hidden-sm-and-up"></v-toolbar-side-icon>
+      <v-toolbar-title>
+      <nuxt-link to="/" tag="span" style="cursor: pointer">LogStock</nuxt-link>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-xs-only">
+        <v-btn flat :to ="item.to" v-for="(item, index) in menuItems" :key="index">
+          <v-icon left>{{item.icon}}</v-icon>
+          {{item.title}}
+        </v-btn>
+        <v-btn flat v-if="userIsAuthenticated" @click="onLogout">
+          <v-icon left>exit_to_app</v-icon>
+          Logout
+        </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+    <v-content>
+      <v-container>
+        <nuxt />
+    </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        sideNav: false,
+      }
+    },
+    computed: {
+      menuItems () {
+        let menuItems = [
+          // { icon: 'chrome_reader_mode', title: 'News', to: '/news'},
+          { icon: 'fa fa-newspaper-o', title: 'News', to: '/news'},
+          { icon: 'attach_money', title: 'Portfolio', to: '/portfolio'},
+          { icon: 'timeline', title: 'Analytics', to: '/analytics'},
+          { icon: 'lock_open', title: 'Sign in', to: '/signin'}
+        ]
+        // if (this.userIsAuthenticated) {
+        //   menuItems = [
+        //   { icon: 'supervisor_account', title: 'View Meetups', to: '/meetup'},
+        //   { icon: 'room', title: 'Organize Meetup', to: '/meetup/new'},
+        //   { icon: 'person', title: 'Profile', to: '/user'}
+        // ]
+        // }
+        return menuItems
+      },
+      userIsAuthenticated () {
+        return true
+      }
+    },
+    methods: {
+      onLogout () {
+        console.log('logout')
+      }
+    } 
+  }
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,108 @@
+<template>
+<v-container>
+  <v-layout row wrap>
+    <v-flex xs12 sm4 class="mb-4">
+      <v-card color="grey darken-2">
+        <v-card-title>
+          <h4 class="headline">Davi Rolim</h4>
+        </v-card-title>
+        <v-card-text>
+          <div class="mb-2">
+            <v-icon left>fas fa-money</v-icon>
+            Patrimônio: R$ 7223.45
+          </div>
+          <div class="mb-2">
+            <v-icon left>timeline</v-icon>
+            <nuxt-link to="/inspire" tag="span" style="cursor: pointer">Variação 5 dias: R$ 85.21</nuxt-link>
+          </div>
+          <div>
+            <v-icon left>attach_money</v-icon>
+            <nuxt-link to="/inspire" tag="span" style="cursor: pointer">Portfólio: GGBR4(16.21), GOAU4(7.76)</nuxt-link>
+          </div>
+        </v-card-text>
+      </v-card>
+    </v-flex>
+    <v-flex xs12 sm7 offset-sm1>
+       <line-chart
+        :data="chartData"
+        :options="options"
+        :width="400"
+        :height="200"
+        >
+      </line-chart>
+    </v-flex>
+    <v-divider></v-divider>
+    <v-flex xs12 sm12 class="text-xs-center mt-4">
+      <v-card>
+        <v-list expand three-line>
+          <v-subheader>Evolução Semanal</v-subheader>
+          <template v-for="(item, index) in items">
+            <v-list-tile avatar :key="item.title">
+              <v-list-tile-content>
+                <v-list-tile-title>{{item.title}}</v-list-tile-title>
+                <v-list-tile-sub-title :class="{'teal lighten-1': item.valor>0, 'red darken-4': item.valor<0}">
+                  <span class="text--primary">{{item.subtitle}} {{item.valor}}</span>
+                </v-list-tile-sub-title>
+              </v-list-tile-content>
+            </v-list-tile>
+            <v-divider :key="index"></v-divider>
+          </template>
+        </v-list>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</v-container>
+</template>
+
+<script>
+import LineChart from '../components/charts/LineChart.js'
+
+export default {
+  components: {
+    LineChart
+  },
+  data () {
+    return {
+      items: [
+          { title: '13/04/2018', subtitle: "GGBR4 0.35% (0.13) / GOAU4 0.78% (0.06) alteração do patromonio:  R$ ", valor: +15.21 },
+          { title: '12/04/2018', subtitle: "GGBR4 2.85% (0.41) / GOAU4 3.24% (0.26) alteração do patromonio:  R$ ", valor: +93.03 },
+          { title: '11/04/2018', subtitle: "GGBR4 -0.79% (-0.12) / GOAU4 0.43% (-0.03) alteração do patromonio: R$ ", valor: -21.57 },
+          { title: '10/04/2018', subtitle: "GGBR4 2.35% (0.33) / GOAU4 2.88% (0.16) alteração do patromonio: R$ ", valor: +73.97 },
+          { title: '09/04/2018', subtitle: "GGBR4 -1.35% (-0.20) / GOAU4 -0.95% (-0.07) alteração do patromonio: R$ ", valor:-47.12 }
+        ],
+      chartData: {
+        labels: ['One', 'Two', 'Three','Four', 'Five'],
+        datasets: [
+          {
+            label: 'Evolution last 5 days',
+            backgroundColor: 'grey',
+            data: [1000.25, 3300.10, 3120.56, 4600.29, 4324.91]
+          }
+        ]
+      },
+      options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true
+            // stepSize: 1,
+          }
+        }],
+        xAxes: [{
+          ticks: {
+            beginAtZero: true
+            }
+          }]
+        }
+      }
+    }
+  },
+  mounted () {
+    this.fillData()
+  },
+  methods: {
+      fillData () {
+    }
+  }
+}
+</script>
